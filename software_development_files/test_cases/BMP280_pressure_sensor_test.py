5e3776958ca55a1fc31e29eadd7bd2c7e2d781d6
R4VIL <ravioliofpitsund4@gmail.com>
2024-03-01 21:28:28 -0800

    update bmp280_pressure_sensor_test.py

--- software_development_files/test_cases/BMP280_pressure_sensor_test.py
+++ software_development_files/test_cases/BMP280_pressure_sensor_test.py
@@ -7,12 +7,25 @@ import bmp280
 from bmp280_configuration import BMP280Configuration
 from bmp280_i2c import BMP280I2C
 
-i2c0_sda = Pin(2)
-i2c0_scl = Pin(3)
-i2c0 = I2C(1, sda=i2c0_sda, scl=i2c0_scl, freq=400000)
-bmp280_i2c = BMP280I2C(0x77, i2c0)  # address may be different
+try:
+    i2c0_sda = Pin(2)
+    i2c0_scl = Pin(3)
+    i2c0 = I2C(1, sda=i2c0_sda, scl=i2c0_scl, freq=400000)
+except Exception as e:
+    print("Error found initializing IC2", e);
+    raise SystemExit
+try:
+    bmp280_i2c = BMP280I2C(0x77, i2c0)  #address may be different
+except Exception as e:
+    print("Error initializing BMP280 sensor");
+    raise SystemExit
 
 while True:
-    readout = bmp280_i2c.measurements
-    print(f"T: {round(readout['t'],2)} °C, P: {round(readout['p'],2)} hPa.")
-    sleep(1)
+    try:
+        readout = bmp280_i2c.measurements
+        print(f"T: {round(readout['t'],2)} °C, P: {round(readout['p'],2)} hPa.")
+    except Exception as e:
+        print("Error found reading sensor data (temperature and pressure)");
+        raise SystemExit
+    finally:
+        sleep(1)
