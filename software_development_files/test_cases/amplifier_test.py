83454eb00bb33effdfb0ecfb7bf7093b78216439
R4VIL <ravioliofpitsund4@gmail.com>
2024-03-28 17:59:41 -0700

    update amplifier, cooler, and add pin relay test

--- software_development_files/test_cases/amplifier_test.py
+++ software_development_files/test_cases/amplifier_test.py
@@ -3,29 +3,29 @@ import time
 #MR amplifier=on, then RF driver=on
 #RF driver=off, then MR amplifier=off
 
-gpio_pin_list = range(16, 22)
+gpio_pin_list = range(16, 23) #excluding pin 23
 for i in gpio_pin_list:
     gpio_pin = Pin(i, Pin.OUT)
-    gpio_pin.off()
+    gpio_pin.on() #no current
     
 # Define GPIO pins
 mr_amp_pin = Pin(16, Pin.OUT)    
 rf_drive_pin = Pin(17, Pin.OUT) 
 
 def turn_on_mr_amplifier():
-    mr_amp_pin.on()
+    mr_amp_pin.off()
     print("MR amplifier is turned on.")
 
 def turn_off_mr_amplifier():
-    mr_amp_pin.off()
+    mr_amp_pin.on()
     print("MR amplifier is turned off.")
 
 def turn_on_rf_drive():
-    rf_drive_pin.on()
+    rf_drive_pin.off()
     print("RF drive is turned on.")
 
 def turn_off_rf_drive():
-    rf_drive_pin.off()
+    rf_drive_pin.on()
     print("RF drive is turned off.")
 
 while True:
