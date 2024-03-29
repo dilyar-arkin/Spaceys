8b61d1dbb71674fff662a46e2dadcbcf82d1d98a
R4VIL <ravioliofpitsund4@gmail.com>
2024-03-28 17:24:08 -0700

    update cooler test

--- software_development_files/test_cases/Peltier_cooler_control_test.py
+++ software_development_files/test_cases/Peltier_cooler_control_test.py
@@ -4,6 +4,7 @@ import time
 # Define GPIO pins
 peltier_pin = Pin(16, Pin.OUT)  # GPIO pin controlling Peltier cooler
 temp_sensor_pin = 28            # ADC pin for temperature sensor
+relay_pin = Pin(22, Pin.OUT)
 
 # Define ADC object for temperature sensor
 #adc.atten(ADC.ATTN_11DB)  # Set attenuation for maximum input voltage of 3.6V
@@ -15,7 +16,7 @@ def read_temperature():
         adc_value = temp36.read_u16()
         volt = (3.3/65535)*adc_value
         DegC = (100*volt) - 50
-        print(round(DegC,2))
+        #print(round(DegC,2))
         return DegC
         
     except Exception as e:
@@ -27,13 +28,15 @@ while True:
     temperature = read_temperature()
     print("Temperature:", temperature, "C")
 
-    if round(temperature) > 40:
+    if round(temperature) > 30:
         # If temperature exceeds 40 degrees Celsius, turn off Peltier cooler
         peltier_pin.off()
+        relay_pin.off()
         print("Peltier cooler turned off due to high temperature")
     else:
         # Otherwise, turn on Peltier cooler
         peltier_pin.on()
+        relay_pin.on()
         
 
     time.sleep(1)  # Sleep for 1 second before reading temperature again
