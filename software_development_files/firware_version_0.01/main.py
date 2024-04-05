274b442b9f83be8efffa0a4664dc38e2cf267949
R4VIL <ravioliofpitsund4@gmail.com>
2024-04-04 18:20:00 -0700

    upload imu.py, vector3d.py, updated sdcard_init_class.py

--- software_development_files/firware_version_0.01/main.py
+++ software_development_files/firware_version_0.01/main.py
@@ -29,15 +29,15 @@ from sdcard_init_class import *
 def main():
     try:
         #initialize I2C bus
-        i2c0_sda = Pin(2)
-        i2c0_scl = Pin(3)
-        i2c0 = I2C(1, sda=i2c0_sda, scl=i2c0_scl, freq=400000)
+        i2c1_sda = Pin(2)
+        i2c1_scl = Pin(3)
+        i2c1 = I2C(1, sda=i2c1_sda, scl=i2c1_scl, freq=400000)
     except Exception as e:
         print("Error found initializing I2C bus: ", e);
         raise SystemExit;
 
     try:
-        bmp280_i2c = BMP280I2C(0x77, i2c0) #address might be different, depending on the sensor
+        bmp280_i2c = BMP280I2C(0x77, i2c1) #address might be different, depending on the sensor
     except Exception as e:
         print("Error found initializing BMP280 sensor: ",e);
         raise SystemExit;
@@ -61,7 +61,7 @@ def main():
             raise SystemExit
         finally:
             sleep(1); #delay for 1 sec between readings
-       
+    
     #SD = sdcard_init('SD',5,7,4,6,0)
     #BMP_sensor = bmp_func('bmp_sensor')
 
